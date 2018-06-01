1fe59428d2553c79eaadf11f1634bdd930b3824a
damcipolat <damian.cipolat@gmail.com>
2018-06-01 01:09:44 -0300

    subo

---
+++ libro_ia/robot.js
@@ -0,0 +1,71 @@
+/* -----------------------------
+    Creado por Damián Cipolat
+  ------------------------------ */
+
+//Enum de estados.
+const estados = {BUSQUEDA:1,NUEVA_BUSQUEDA:2,IR_BATERIA:3,RECARGAR:4,MUERTO:5,ALEATORIO:6};
+
+//Variables internas.
+let robot = {energia:400};
+
+//Estado inicial.
+let estadoInicial = estados.APAGADO;
+
+//Temperatura.
+let tempSensor = 0;
+
+//Temperatura actual al azar.
+const tempActualRandom = ()=>{
+
+  let max = 100;
+  let min = 10;
+
+  return Math.round(Math.random()*(max-min)+min);
+
+}
+
+//Función de iteración.
+const bucleCheck = ()=>{
+
+  //Traigo la tempertaura actual.
+  tempSensor = tempActualRandom();
+
+  //Prender.
+  if ((estadoInicial==estados.APAGADO)&&(tempSensor<50)){
+
+    encender();
+    estadoInicial = estados.ENCENDIDO;
+
+  }
+
+  //Apagar.
+  if ((estadoInicial==estados.ENCENDIDO)&&(tempSensor>85)){
+
+    apagar();
+    estadoInicial = estados.APAGADO;
+    
+  }
+
+
+}
+
+//Funciones de cada transición.
+const apagar = ()=>{
+
+  console.log('---> Apagar!');
+
+}
+
+const encender = ()=>{
+
+  console.log('---> Encender!');
+
+}
+
+//Bucle cada 3 segundos.
+let bucle = setInterval(bucleCheck, 2000);
+
+//Inicio.
+console.log('> Ejemplo maquina de estado - sensor de tempertaura');
+console.log('');
+bucleCheck();
